334b11a00802db06687ec8d0d48774fa1ac7e790
Radostin Paspalov <rrr@zzz.bg>
2018-11-26 22:06:24 +0100

    commit 4 in branche2

--- src/Magare.java
+++ src/Magare.java
@@ -3,5 +3,8 @@ public class Magare {
     public static void main(String[] s) {
         System.out.println("Magare");
         System.out.println("Magare 2");
+
+        Kon k = new Kon();
+        k.printme();
     }
 }
